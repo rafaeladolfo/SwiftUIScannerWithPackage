8a96f1fe2a4c938f7490d3e7962c632f00f96e6f
Rafael Adolfo <rafadev1989@gmail.com>
2020-03-30 19:53:20 -0300

    Add style + handling from package

---
+++ SwiftUIScannerWithPackage/SwiftUIScannerWithPackage/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  SwiftUIScannerWithPackage
+//
+//  Created by Rafael Adolfo on 30/03/20.
+//  Copyright © 2020 Rafael Adolfo. All rights reserved.
+//
+
+import SwiftUI
+import CodeScanner
+
+struct ContentView: View {
+    @State var scannedCode = ""
+    @State var scannerIsActive = false
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        self.scannerIsActive = false
+        switch result {
+        case .success(let code):
+            self.scannedCode = "Resultado:" + code
+        case .failure( _):
+            print("Scanning failed")
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(scannedCode)
+                .fontWeight(Font.Weight.semibold)
+                .lineLimit(nil)
+            
+            
+            Button(action: {
+                self.scannerIsActive = true
+            }) {
+                   HStack {
+                       Image(systemName: "faceid")
+                       Text("Escanear")
+                   }
+                .padding(.all, 12)
+                .foregroundColor(.white)
+                .background(Color.blue)
+            }
+            .sheet(isPresented: $scannerIsActive) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "It's alive!", completion: self.handleScan)
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
